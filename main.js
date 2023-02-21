33e6ef9de17e3bb1bc19b30049acd1a78710bf65
Rijad-A <rijadajeti234@gmail.com>
2023-02-21 17:33:06 +0100

    grrr
    rrrrrr

--- main.js
+++ main.js
@@ -32,51 +32,51 @@ function showSlides(n) {
 
 alert("Welcome to our page");
 
-const items = document.querySelectorAll('.card');
+// const items = document.querySelectorAll('.card');
 
-document.querySelector('#breakfastBtn').addEventListener('click', function() {
-    items.forEach(item => {
-        // console.log(item);
-        if (item.id != 'breakfast') {
-            item.style.display = 'none';
-        } else {
-            item.style.display = 'grid';
-        }
-    });
-});
-document.querySelector('#lunchBtn').addEventListener('click', function() {
-    items.forEach(item => {
-        // console.log(item);
-        if (item.id != 'lunch') {
-            item.style.display = 'none';
-        } else {
-            item.style.display = 'grid';
-        }
-    });
-});
-document.querySelector('#shakesBtn').addEventListener('click', function() {
-    items.forEach(item => {
-        // console.log(item);
-        if (item.id != 'shakes') {
-            item.style.display = 'none';
-        } else {
-            item.style.display = 'grid';
-        }
-    });
-});
-document.querySelector('#dinnerBtn').addEventListener('click', function() {
-    items.forEach(item => {
-        // console.log(item);
-        if (item.id != 'dinner') {
-            item.style.display = 'none';
-        } else {
-            item.style.display = 'grid';
-        }
-    });
-});
+// document.querySelector('#breakfastBtn').addEventListener('click', function() {
+//     items.forEach(item => {
+//         // console.log(item);
+//         if (item.id != 'breakfast') {
+//             item.style.display = 'none';
+//         } else {
+//             item.style.display = 'grid';
+//         }
+//     });
+// });
+// document.querySelector('#lunchBtn').addEventListener('click', function() {
+//     items.forEach(item => {
+//         // console.log(item);
+//         if (item.id != 'lunch') {
+//             item.style.display = 'none';
+//         } else {
+//             item.style.display = 'grid';
+//         }
+//     });
+// });
+// document.querySelector('#shakesBtn').addEventListener('click', function() {
+//     items.forEach(item => {
+//         // console.log(item);
+//         if (item.id != 'shakes') {
+//             item.style.display = 'none';
+//         } else {
+//             item.style.display = 'grid';
+//         }
+//     });
+// });
+// document.querySelector('#dinnerBtn').addEventListener('click', function() {
+//     items.forEach(item => {
+//         // console.log(item);
+//         if (item.id != 'dinner') {
+//             item.style.display = 'none';
+//         } else {
+//             item.style.display = 'grid';
+//         }
+//     });
+// });
 
-document.querySelector('#allBtn').addEventListener('click', function() {
-    items.forEach(element => {
-        element.style.display = 'grid';
-    });
-})
+// document.querySelector('#allBtn').addEventListener('click', function() {
+//     items.forEach(element => {
+//         element.style.display = 'grid';
+//     });
+// })
