ea1fcb56fe13d5c5f3945ce50321cfc3c62d40c4
Rijad-A <rijadajeti234@gmail.com>
2023-02-23 15:38:48 +0100

    e fundit
    1

--- main2.js
+++ main2.js
@@ -2,83 +2,83 @@ let foodContainer = document.querySelector('.food-container');
 
 const fooditem=[
     {
-        FoodName: "Cheese paratha",
-        foodimg: 'images/b1.jpg',
-        price: "RS 250",
+        FoodName: "Onion Pizza",
+        foodimg: 'img/pizza1.jfif',
+        price: "10.99 EUR",
         type: "breakFast",
-        des: "These cheese flatbreads make for a wholesome breakfast or brunch. There are many many varieties of paratha made and this is one such tasty paratha which the kids are going to love."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
     {
-        FoodName: "Aloo Paratha",
-        foodimg: 'images/b2.jpg',
-        price: "RS 200",
+        FoodName: "Healthy dish",
+        foodimg: 'img/food2.jpeg',
+        price: "5.99 EUR",
         type: "breakFast",
-        des: "Aloo Paratha is a bread dish originating from the Indian subcontinent. It is a breakfast dish originated from the Punjab region."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
     {
-        FoodName: "Egg Paratha",
-        foodimg: 'images/b3.jpg',
-        price: "RS 150",
+        FoodName: "Eggs",
+        foodimg: 'img/food1.jpeg',
+        price: "7.99 EUR",
         type: "breakFast",
-        des: "Egg paratha recipe — easy breakfast, brunch or dinner recipe that is protein rich, healthy and tasty. Egg paratha is a kids' favorite, so i make it often for their after school snack. To make egg paratha, you can use ready paratha, roti or frozen paratha as well."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
     {
-        FoodName: "Daal Chawal",
-        foodimg: 'images/l1.jpg',
-        price: "RS 400",
+        FoodName: "Chicken Menu",
+        foodimg: 'img/food4.jfif',
+        price: "16.99 EUR",
         type: "Lunch",
-        des: "The most fascinating, fabulously fantastic and simple lightweight Pakistan’s most favorite dish, Daal Chawal Recipe (Daal Rice) is here at Sooperchef.pk. It’s simple, fast and delicious Pakistani Recipe."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
     {
-        FoodName: "Pakal Paneer",
-        foodimg: 'images/l2.jpg',
-        price: "RS 400",
+        FoodName: "Combination Menu",
+        foodimg: 'img/food5.jpg',
+        price: "13.99 EUR",
         type: "Lunch",
-        des: "Palak Paneer is a popular Indian dish of Indian cottage cheese cubes in a mild, spiced smooth spinach sauce. This delicious creamy dish is made with fresh spinach leaves, paneer (firm cottage cheese), onions, tomatoes, herbs and spices."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
     {
-        FoodName: "Aloo Ghobi",
-        foodimg: 'images/l3.jpg',
-        price: "RS 200",
+        FoodName: "Hamburger",
+        foodimg: 'img/food6.jpg',
+        price: "3.99 EUR",
         type: "Lunch",
-        des: "Aloo Gobi ki Sabzi is a classic Pakistani dish of Potatoes and Cauliflower cooked in savory spices and garnished with chillies and cilantro."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
     {
         FoodName: "Aloo Goshat",
-        foodimg: 'images/l4.jpg',
-        price: "RS 200",
+        foodimg: 'img/food7.jpg',
+        price: "4.59 EUR",
         type: "Lunch",
-        des: "Aloo Gosht is a spicy, thick and creamy meat gravy that originates from Pakistani and North Indian cuisine.This gravy has potatoes cooked with lamb or mutton in a thick stew."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i."
     },
 
     {
         FoodName: "Banana Shakes",
-        foodimg: 'images/s1.jpg',
-        price: "RS 150",
+        foodimg: 'img/food8.webp',
+        price: "2.99 EUR",
         type: "Shakes",
-        des: "You need less than five minutes to make this easy banana smoothie. Use our simple recipe on it’s own or use it as a base for other smoothies."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
     {
         FoodName: "Mango Shakes",
-        foodimg: 'images/s2.jpg',
-        price: "RS 150",
+        foodimg: 'img/food9.jpeg',
+        price: "2.99 EUR",
         type: "Shakes",
-        des: "Mango shake is a cool and tempting fruit drink prepared by simply blending ripe mango pieces, milk and sugar. To keep things simple and easy, this recipe primarily explains how to make mango shake with milk."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
 
     {
-        FoodName: "Biryani",
-        foodimg: 'images/d1.jpg',
-        price: "RS 600",
+        FoodName: "Combination2",
+        foodimg: 'img/food10.jpg',
+        price: "13.99 EUR",
         type: "Dinner",
-        des: "Biryani is a mixed rice dish originating among the Muslims of the Indian subcontinent. It is made with Indian spices, rice, and meat, and sometimes, in addition, eggs and/or vegetables such as potatoes in certain regional varieties."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     },
     {
-        FoodName: "Korma",
-        foodimg: 'images/d2.jpg',
-        price: "RS 250",
+        FoodName: "Sphagetti",
+        foodimg: 'img/food11.jpg',
+        price: "7,99 EUR",
         type: "Dinner",
-        des: "Korma is a gravy dish that is usually made with yogurt, lots of nuts and spices. A Mughal era original, Chicken Korma is the perfect dinner party dish that is easy, quick and a no fuss recipe."
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo augue non libero varius, a congue diam tempor. Integer euismod, lacus a luctus commodo, diam nulla commodo urna, non varius nulla tellus at velit. In hac habitasse platea dictumst. Aliquam bibendum, nibh i"
     }
 ]
 
